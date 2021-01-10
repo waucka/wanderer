06c3a0a86c6382dfa02a2d36f21ffef862f26e5a
Alexander Wauck <waucka@protonmail.com>
2021-01-10 15:16:33 -0600

    Compile shaders for Vulkan 1.2 instead of 1.1

--- build_shaders.sh
+++ build_shaders.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-COMMON_ARGS="--target-env vulkan1.1"
+COMMON_ARGS="--target-env vulkan1.2"
 
 glslangValidator -V lighting.vert.glsl $COMMON_ARGS -o lighting.vert.spv || exit 1
 glslangValidator -V lighting.frag.glsl $COMMON_ARGS -o lighting.frag.spv || exit 1
